6a12ce1ce10bcba8ff9062ab1a511ba361ec3f34
esma <esma.ea.aksoy@gmail.com>
2023-12-02 19:48:47 +0300

    header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
+import Header from "./component/Header/Header";
 
 function App() {
   return (
     <div>
-     
+     <Header/>
     </div>
   );
 }
---
+++ src/component/Header/Header.jsx
@@ -0,0 +1,8 @@
+import "./Header.css"
+const Header = () => {
+  return <header>
+    <img src="https://cdn.pixabay.com/photo/2016/09/10/17/18/book-1659717_640.jpg" alt="" />
+    <p>Michel Foucault</p>
+  </header>;
+};
+export default Header;
