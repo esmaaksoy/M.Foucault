705785e749f0d00b40d8c800ec4400a743a9f6ad
esma <esma.ea.aksoy@gmail.com>
2023-12-05 18:15:36 +0300

    add button

--- src/component/BookCard/BookCard.jsx
+++ src/component/BookCard/BookCard.jsx
@@ -1,16 +1,24 @@
+import { useState } from "react";
 import { data } from "../../helper/data";
 import BookStyle from "./BookCard.module.css";
 const BookCard = () => {
+  const [clear, setClear]= useState(data)
   return (
     <main>
     <div className={BookStyle.container}>
-      {data.map(({ image, name, id }) => (
+    <h1>{clear.length} Books of Michel Foucault</h1>
+      <div className={BookStyle.cards}>
+      {clear.map(({ image, name, id }) => (
         <div key={id} className={BookStyle.card}>
           <img className={BookStyle.image} src={image} alt={name} />
           <p className={BookStyle.name}>{name}</p>
         </div>
       ))}
-    </div>
+        <button onClick={()=>setClear([])}>Clear All</button>
+      </div>
+    
+      </div>
+      
     </main>
   );
 };
